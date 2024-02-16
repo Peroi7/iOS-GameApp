7f162a1a0ccb51704a5c2b787dc1c63e9835608f
Peroi7 <ivic.petar77@gmail.com>
2024-02-16 17:16:52 +0100

    Added HUD library,  BaseOnboardingViewController, UI update

---
+++ ios-GameApp/BaseOnboardingViewController.swift
@@ -0,0 +1,78 @@
+//
+//  BaseOnboardingViewController.swift
+//  ios-GameApp
+//
+//  Created by Pero Ivic on 16/02/2024.
+//
+
+import UIKit
+import PureLayout
+import IHProgressHUD
+
+class BaseOnboardingViewController: UIViewController {
+    
+    private enum Constants {
+        static let baseOnboardingNavigationTitle = "Genres"
+    }
+    
+    var navigationItemTitle: String { return Constants.baseOnboardingNavigationTitle }
+    
+    private var separatorView: UIView!
+    private let collectionView: UICollectionView = {
+        let collectionViewLayout = UICollectionViewFlowLayout()
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: collectionViewLayout)
+        return collectionView
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupNavigationBarAppearance()
+        addSeparatorView()
+        navigationItem.title = navigationItemTitle
+    }
+    
+    private func addSeparatorView() {
+        separatorView = UIView()
+        separatorView.backgroundColor = .white
+        guard let navigationBar = navigationController?.navigationBar else { return }
+        navigationBar.addSubview(separatorView)
+        separatorView.autoPinEdge(.top, to: .bottom, of: navigationBar)
+        separatorView.autoPinEdge(.left, to: .left, of: navigationBar)
+        separatorView.autoPinEdge(.right, to: .right, of: navigationBar)
+        separatorView.autoSetDimension(.height, toSize: 1)
+    }
+    
+    func showHud() {
+        IHProgressHUD.show()
+    }
+    
+    func dismissHud() {
+        IHProgressHUD.dismiss()
+    }
+    
+    }
+    
+    //MARK: - CollectionView
+    
+    
+
+//MARK: - NavigationBar Appearance
+
+extension BaseOnboardingViewController {
+    
+    func setupNavigationBarAppearance() {
+        if #available(iOS 15, *) {
+            let appearance = UINavigationBarAppearance()
+            appearance.configureWithTransparentBackground()
+            appearance.backgroundColor = Colors.primaryBackground
+            appearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+            appearance.shadowColor = .white
+            navigationController?.navigationBar.standardAppearance = appearance;
+            navigationController?.navigationBar.scrollEdgeAppearance = navigationController?.navigationBar.standardAppearance
+        } else {
+            navigationController?.navigationBar.prefersLargeTitles = true
+            navigationController?.navigationBar.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+            navigationController?.navigationBar.isTranslucent = false
+        }
+    }
+}
---
+++ ios-GameApp/Colors.swift
@@ -0,0 +1,14 @@
+//
+//  Colors.swift
+//  ios-GameApp
+//
+//  Created by Pero Ivic on 16/02/2024.
+//
+
+import UIKit
+
+struct Colors {
+    
+    static let primaryBackground = UIColor(named: "#151515")
+    
+}
