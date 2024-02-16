16c3db9f24b80f74e6f89a20a60d332a8e5e9363
Peroi7 <ivic.petar77@gmail.com>
2024-02-16 17:16:52 +0100

    Added HUD library,  BaseOnboardingViewController, UI update

--- ios-GameApp/ViewController.swift
+++ ios-GameApp/ViewController.swift
@@ -7,15 +7,23 @@
 
 import UIKit
 
-class BaseViewController: UIViewController {
+class ViewController: BaseOnboardingViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
-        view.backgroundColor = .white
-    }
 
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
 
 }
-
