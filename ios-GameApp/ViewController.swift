0cc7da3b7eff17eb2cc0f9fcebe6f4f0789ecc70
Peroi7 <ivic.petar77@gmail.com>
2024-02-16 15:49:54 +0100

    Initial project setup, PureLayout added with SPM

---
+++ ios-GameApp/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  ios-GameApp
+//
+//  Created by Pero Ivic on 16/02/2024.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .white
+    }
+
+
+}
+
